8ec579b86c629f2c7c5c19c922613d1b9a9f7156
SculptTechProject <sculpttechproject@gmail.com>
2024-09-11 23:11:13 +0200

    implemented if time is less than 4 bg will be red

--- frontend/src/components/ContractorsList.tsx
+++ frontend/src/components/ContractorsList.tsx
@@ -9,36 +9,41 @@ const ContractorsList: React.FC<ContractorsListProps> = ({
   contractors,
   onDeleteContractor,
 }) => {
+  const today = new Date(); // Dzisiejsza data
+
   return (
     <div className="flex justify-center py-8">
       <ul className="py-8">
         {contractors.length > 0 ? (
-          contractors.map((contractor) => (
-            <li className="" key={contractor._id}>
-              <strong>{contractor.name}</strong> - {contractor.nip} -{" "}
-              {contractor.phone} | {contractor.address} | {contractor.notes} |{" "}
-              <strong>
-                {contractor.contactDate
-                  ? // Formatowanie daty tutaj
-                    new Date(contractor.contactDate).toLocaleDateString(
-                      "pl-PL",
-                      {
-                        year: "numeric",
-                        month: "2-digit",
-                        day: "2-digit",
-                      }
-                    )
-                  : "No contact date"}{" "}
-              </strong>
-              |{" "}
-              <button
-                className="px-5 py-3 mx-8 bg-red-400 text-"
-                onClick={() => onDeleteContractor(contractor._id)}
-              >
-                Delete
-              </button>
-            </li>
-          ))
+          contractors.map((contractor) => {
+            const contactDate = new Date(contractor.contactDate); // Konwersja daty kontrahenta
+            const timeDifference = contactDate.getTime() - today.getTime(); // Różnica w milisekundach
+            const daysLeft = Math.ceil(timeDifference / (1000 * 3600 * 24)); // Różnica w dniach
+
+            // Ustawiamy kolor tła na czerwony, jeśli dni do kontaktu jest mniej niż 4
+            const backgroundColor = daysLeft <= 4 ? "bg-red-400" : "bg-white";
+
+            return (
+              <li className={`${backgroundColor} p-4`} key={contractor._id}>
+                <strong>{contractor.name}</strong> - {contractor.nip} -{" "}
+                {contractor.phone} | {contractor.address} | {contractor.notes} |{" "}
+                <strong>
+                  {contactDate.toLocaleDateString("pl-PL", {
+                    year: "numeric",
+                    month: "2-digit",
+                    day: "2-digit",
+                  })}{" "}
+                </strong>
+                |
+                <button
+                  className="px-5 py-3 mx-8 text-white bg-red-600"
+                  onClick={() => onDeleteContractor(contractor._id)}
+                >
+                  Delete
+                </button>
+              </li>
+            );
+          })
         ) : (
           <p className="text-red-600">No contractors found</p>
         )}

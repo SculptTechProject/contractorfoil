b6c75d71ad61e7b1f69dba017744405f8c7aa223
SculptTechProject <sculpttechproject@gmail.com>
2024-11-18 19:50:25 +0100

    changed footer animations

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -8,14 +8,14 @@ const Footer = () => {
             <div className="footer-content">
 
                 {/* Left Column */}
-                <div className="footer-brand">
+                <div className="footer-brand" data-aos="fade-up">
                     <h3>ContractorFoil</h3>
                     <p>Manage contractors with ease and efficiency.</p>
                 </div>
 
                 {/* Center Column */}
                 <div className="footer-links">
-                    <div>
+                    <div data-aos="fade-right">
                         <h4>Explore</h4>
                         <ul>
                             <li><a href="#" className="footer-link">Home</a></li>
@@ -24,7 +24,7 @@ const Footer = () => {
                             <li><a href="#" className="footer-link">Pricing</a></li>
                         </ul>
                     </div>
-                    <div>
+                    <div data-aos="fade-left">
                         <h4>Support</h4>
                         <ul>
                             <li><a href="#" className="footer-link">Help Center</a></li>
@@ -36,7 +36,7 @@ const Footer = () => {
                 </div>
 
                 {/* Right Column */}
-                <div className="footer-social">
+                <div className="footer-social" data-aos="fade-up">
                     <h4>Follow Us</h4>
                     <div className="footer-icons">
                         <a href="#" className="footer-icon"><FaFacebook /></a>
